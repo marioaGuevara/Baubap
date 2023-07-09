ec3d806e4956f04041b80fc7c4170a6fb7857ca2
mario <mario.guevara.c@gmail.com>
2023-07-09 13:53:31 -0600

    renamed

---
+++ pages/SQL Challenge.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+import gdown
+import sqlite3
+from helpers.queries import query1, query2, query3,query4
+from helpers.auth import check_password
+
+if check_password():
+    # url = 'https://drive.google.com/uc?export=download&id=1Wq0TQJAgy25FnVRQq9uu-lL3ko281baa'
+    output = 'Baubap Mysql Challenge'
+    # gdown.download(url, output, quiet=False)
+    db_data = pd.read_csv(output)
+    st.set_page_config(layout="wide")
+
+    DB = sqlite3.connect("BAUABAP CHALLENGE.db")
+    db_cursor = DB.cursor()
+    # db_data.to_sql(name= "loans",con=DB)
+
+    t1, t2, t3 = st.tabs(['Exercise 1', 'Exercise 2', 'Exercise 3'])
+
+    with t1:
+        st.header("Q: Calculate the percentage of loans that were declined in the month of April 2019.")
+        st.write(" ")
+        st.header("Answer")
+        declinedDf = pd.read_sql_query(con=DB,sql=query1)
+
+        aprilDecline = declinedDf.loc[declinedDf['MONTH'] == '2019-04-01'].DECLINED_LOANS_PERC[0]
+        marchDecline = declinedDf.loc[declinedDf['MONTH'] == '2019-03-01'].DECLINED_LOANS_PERC[1]
+        st.metric('Decline Rate: April, 2019', "{0:.2%}".format(round(aprilDecline,4)), str(round((aprilDecline-marchDecline)*100,2))+"bps MoM", delta_color="inverse")
+        
+        st.subheader("Here's the query I used")
+        st.write("I could've used an interval to make it dynamic, however for our purposes, this seems good.")
+        st.code(query1)
+        st.subheader("The result I got")
+        st.write(declinedDf)
+
+    # 
+    with t2:
+        st.header("Q: Create a table showing for each user their most recent loanId, their previous loanId, and the difference in days between those two loans.")
+        st.write(" ")
+        st.header("Answer")
+        st.dataframe(pd.read_sql_query(con=DB,sql=query2))
+
+        st.write(" ")
+        st.subheader("Here's the code")
+        st.code(query2)
+
+
+    with t3: 
+        st.header('I wanted to see if there was a correlation between loan amount and the payment likelyhood')
+        st.subheader("So first I ran this query to find out the quartiles based on loan amount")
+        st.code(query3)
+        st.write(pd.read_sql_query(con=DB,sql=query3).describe())
+
+        st.subheader("Then I divided the data into quartiles, and calculated the % payment percent")
+        st.code(query4)
+
+        st.write(pd.read_sql_query(con=DB,sql=query4))
