0608d956d1534196c38687a0f08385a79946fe0a
mario <mario.guevara.c@gmail.com>
2023-07-09 18:34:10 -0600

    second answer ready

--- pages/Python Challenge.py
+++ pages/Python Challenge.py
@@ -65,8 +65,8 @@ with t2:
             return 'Tablet User'
         elif row['usaTabletParaAcceder'] == 1 and row['usaMovilParaAcceder'] == 1:
             return 'Tablet + Mobile'
-        else:
-            return 'Other'
+    
+    st.header("How's the contact rate by Platform?")
     dfIssiesWithPlatform = df[['haContactoASoporte', 'usaTabletParaAcceder', 'usaMovilParaAcceder', 'CustomerID', 'usuarioPerdido']]
     dfIssiesWithPlatform['haContactoASoporte'] = np.where(dfIssiesWithPlatform['haContactoASoporte'] == 'Sí', 1, 0)
     dfIssiesWithPlatform['usaTabletParaAcceder'] = np.where(dfIssiesWithPlatform['usaTabletParaAcceder'] == 'Sí', 1, 0)
@@ -116,21 +116,55 @@ with t2:
             # yaxis3=dict(title='Churn Rate', overlaying='y2', side='right')
 
         )
-    fig = go.Figure(data=[bar_trace,line_trace,line_trace_churn], layout=layout)
-    
+    fig = go.Figure(data=[bar_trace,line_trace], layout=layout)
+    st.subheader("This could mean that our users are having a hard time with our platform, maybe our app isn't working properly.")
     st.plotly_chart(fig, use_container_width=True)
     
     dfIssiesWithPlatform
+
+    st.subheader("How bad for business is this? Let's examine churn by platform, broken down by the users that have contacted support at some point")
+    
 
     dfIssiesWithPlatformRaw['usuarioPerdido'] = df['usuarioPerdido']
     dfIssiesWithPlatformRaw['haContactoASoporte'] =  np.where(df['haContactoASoporte'] == "Sí", 1, 0)
     dfIssiesWithPlatformRaw['usaTabletParaAcceder'] =  np.where(df['usaTabletParaAcceder'] == "Sí",1,0)
 
     dfIssiesWithPlatformRaw['usaMovilParaAcceder'] =  np.where(df['usaMovilParaAcceder'] == "Sí",1,0)
-    dfIssiesWithPlatformRaw = dfIssiesWithPlatformRaw.reset_index()
-    dfIssiesWithPlatformRaw
 
+    dfIssiesWithPlatformRaw = dfIssiesWithPlatformRaw.groupby(['Access Via', 'haContactoASoporte']).agg({'CustomerID': 'count', 'usuarioPerdido': 'sum'})
+
+    dfIssiesWithPlatformRaw = dfIssiesWithPlatformRaw.reset_index()
+
+    dfIssiesWithPlatformRaw['Churn Rate'] = (dfIssiesWithPlatformRaw['usuarioPerdido'] /  dfIssiesWithPlatformRaw['CustomerID'].astype(float)).apply(lambda x: "{:.2%}".format(x))
     
 
+    churn_w_contact = go.Scatter(x=dfIssiesWithPlatformRaw['Access Via'].loc[dfIssiesWithPlatformRaw['haContactoASoporte'] == 0], 
+                            y=dfIssiesWithPlatformRaw['Churn Rate'].loc[dfIssiesWithPlatformRaw['haContactoASoporte'] == 0], 
+                            textposition='top center', 
+                            mode='lines+markers', 
+                            name='Not contacted', 
+                            text=dfIssiesWithPlatformRaw['Churn Rate'].loc[dfIssiesWithPlatformRaw['haContactoASoporte'] == 0],
+                            yaxis='y'
+                            )    
+    churn_wo_contact = go.Scatter(x=dfIssiesWithPlatformRaw['Access Via'].loc[dfIssiesWithPlatformRaw['haContactoASoporte'] == 1], 
+                            y=dfIssiesWithPlatformRaw['Churn Rate'].loc[dfIssiesWithPlatformRaw['haContactoASoporte'] == 1], 
+                            textposition='top center', 
+                            mode='lines+markers', 
+                            name='Contacted', 
+                            text=dfIssiesWithPlatformRaw['Churn Rate'].loc[dfIssiesWithPlatformRaw['haContactoASoporte'] == 1],
+                            yaxis='y'
+                            )    
+
+    layout = go.Layout(
+            yaxis=dict(title='Churn by platform')
+            # yaxis3=dict(title='Churn Rate', overlaying='y2', side='right')
+
+        )
+    fig = go.Figure(data=[churn_w_contact,churn_wo_contact], layout=layout)
+    st.plotly_chart(fig, use_container_width=True)
+    st.subheader('As we can see here, people that contacted have a higher retention rate, this can mean 2 things.')
+    st.write('1.- Our users need more education on how to use our platform, they need like we are right by their side as they are joining our paltform')
+    st.write("2.- We need to improve upon our UI/UX, for some reason some users seem to be getting lost somewhere along the user journey.")
+    dfIssiesWithPlatformRaw 
 
     
